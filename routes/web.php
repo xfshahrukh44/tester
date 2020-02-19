28616f2861155f75364e51dc2020d8df3e6c7137
hammadpirzada1 <hammadpirzada1@gmail.com>
2020-02-19 14:56:53 +0500

    current

--- routes/web.php
+++ routes/web.php
@@ -15,24 +15,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Route::view('/admin', 'admin/dashboard/index');
 Route::view('/admin2', 'admin/dashboard/index2');
 Route::view('/admin3', 'admin/dashboard/index3');
-=======
-Route::get('/admin2', function(){
-    return view('admin/index2');
-});
-
-Route::view('/admin', 'admin/layouts/app');
-Route::view('/admin2', 'admin/index2');
-Route::view('/admin3', 'admin/index3');
->>>>>>> d8acdbd0daea84b6c4ff0712ea271c97c78029ba
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-// Route::group(['middleware' => ['auth']], function () {
+//Route::group(['middleware' => ['auth']], function () {
 
 // });
